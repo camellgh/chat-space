3e6a09df75b02466a12b998fe5e97f8d781fae56
camellgh <sawashita@sawashitaryou-no-MacBook-Pro.local>
2019-04-29 16:05:03 +0900

    messageの非同期

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,53 @@
+$(function(){
+  function buildHTML(message){
+    var image = message.image ? message.image :
+    alt =""
+
+    var html =  `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                         ${ message.user_name }
+                    </div>
+                      <div class="upper-message__date">
+                         ${ message.time } 
+                      </div>
+                    </div>
+                  <div class="lower-meesage">
+                      <p class="lower-message__content">
+                         ${ message.content } 
+                      </p>
+                  </div>
+                  <image src="${ image }">
+                </div>`
+
+    console.log(html);
+    return html;
+  }
+  // var image = message.image ? `<img src="${message.image}">` : ``
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+    })
+    .done(function(data){
+     var html = buildHTML(data);
+      $('.messages').append(html)
+      // $('#message_content').val('')
+      // $('#message_image').val('')
+      $("form")[0].reset();
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    })
+
+    .fail(function(){
+      alert('エラー');
+    })
+     return false;
+  })
+})
