17766b75eaddaf94a906c27e5904430265ff76e9
camellgh <sawashita@sawashitaryou-no-MacBook-Pro.local>
2019-05-01 20:07:24 +0900

    fix3

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -46,10 +46,10 @@ $('#user-search-field').on('keyup', function(e){
 
 function addUser(user_name, user_id) {
   var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-  <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
-  <p class='chat-group-user__name'>${ user_name }</p>
-  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
-</div>`
+              <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+                <p class='chat-group-user__name'>${ user_name }</p>
+                <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+              </div>`
 $('#chat-group-users').append(html);
 
 }
