3448b2e923234184d2db8d89d147dcfcf0969239
camellgh <sawashita@sawashitaryou-no-MacBook-Pro.local>
2019-05-03 09:45:49 +0900

    自動更新の実装

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,36 +1,41 @@
 $(function(){
   function buildHTML(message){
-    var image = message.image ? message.image :""
+    var image = ""
+    if(message.image.url != null){
+      image = `<img class="lower-message__image" src=${message.image.url}>`
+    }
+    console.log(image)
+    var html = `<div class='message' data-id="${message.id}">
+      <div class='upper-message'>
+        <div class='upper-message__user-name'>
+        ${ message.user_name }
+        </div>
+      <div class='upper-message__date'>
+      ${ message.created_at } 
+      </div>
+    </div>
+      <div class='lower-meesage'>
+        <p class='lower-message__content' data-massege-content="${message.id}">
+        ${ message.content }
+        </p>
 
-    var html =  `<div class="message">
-                  <div class="upper-message">
-                    <div class="upper-message__user-name">
-                         ${ message.user_name }
-                    </div>
-                      <div class="upper-message__date">
-                         ${ message.time } 
-                      </div>
-                    </div>
-                  <div class="lower-meesage">
-                      <p class="lower-message__content">
-                         ${ message.content } 
-                      </p>
-                  </div>
-                  <image src="${ image }">
-                </div>`
+      </div>
+      ${ image } 
+    </div>`
     return html;
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
+    var url = window.location.href;
+    console.log(url);
+    $.post({
       type: "POST",
+      url: url,
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false,
+      contentType: false
     })
     .done(function(data){
      var html = buildHTML(data);
@@ -44,4 +49,90 @@ $(function(){
     })
      return false;
   })
+
+  function buildMessageHTML(message) {
+    console.log(message)
+    if (message.content && message.image_url) {
+      var html = `<div class="message" data-id="${message.id}">
+      <div class="upper-message">
+        <div class="upper-message__user-name">
+          ${ message.user_name }
+        </div>
+        <div class="upper-message__date">
+          ${ message.created_at}
+        </div>
+      </div>
+      <div class="lower-meesage">
+          <p class="lower-message__content" data-massege-content="${ message.id }">
+            ${ message.content }
+          </p>
+        <%= image src=${ message.image_url}, class='lower-message__image' %>
+      </div>
+    </div>`
+    } else if (message.content) {
+      var html = `<div class="message" data-id="${message.id}">
+      <div class="upper-message">
+        <div class="upper-message__user-name">
+          ${ message.user_name }
+        </div>
+        <div class="upper-message__date">
+          ${ message.created_at }
+        </div>
+      </div>
+      <div class="lower-meesage">
+          <p class="lower-message__content" data-massege-content="${ message.id }">
+            ${ message.content }
+          </p>
+      </div>
+    </div>`
+    } else if (message.image_url) {
+      var html = `<div class="message" data-id="${message.id}">
+      <div class="upper-message">
+        <div class="upper-message__user-name">
+          ${ message.user_name }
+        </div>
+        <div class="upper-message__date">
+          ${ message.created_at }
+        </div>
+      </div>
+      <div class="lower-meesage">
+        <%= image src=${ message.image_url }, class='lower-message__image' %>
+      </div>
+    </div>`
+    };
+    return html;
+
+  }
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('p:last').data("massege-content");
+    console.log($('p:last'))
+    group_id = location.pathname.split("/messages")
+    console.log(group_id[0] + '/api/messages')
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: group_id[0] + '/api/messages',
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      // console.log(messages);
+      
+      var insertHTML = '';
+      messages.forEach(function(message){
+        // var name = message.content
+        var insertHTML = buildHTML(message);
+      $('.messages').append(insertHTML)     
+    })
+
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 5000);
 })
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,11 @@
-json.content @message.content
-json.image   @message.image_url
+# json.content @message.content
+# json.image   @message.image_url
 json.user_id @message.user.id
+# json.user_name @message.user.name
+json.created_at    @message.created_at.strftime("%Y/%m/%d %R")
+json.(@message, :content, :image)
+# json.created_at format_posted_time(@message.created_at)
 json.user_name @message.user.name
-json.time    @message.created_at.strftime("%Y/%m/%d %R")
+json.image_url @message.image.url
+#idもデータとして渡す
+json.id @message.id
